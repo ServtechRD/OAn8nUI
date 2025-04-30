883c59ff747f3339bf0dfcc8a652c69aacaad1e2
jason <awkwardkao@gmail.com>
2025-04-30 18:00:47 +0800

    fix bug

--- src/index.js
+++ src/index.js
@@ -1,19 +1,37 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { AuthProvider } from "./contexts/AuthContext";
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#1976d2",
+    },
+    secondary: {
+      main: "#dc004e",
+    },
+  },
+});
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
 // 如果您想要开始测量应用程序的性能，請傳遞一個函數
 // 來記錄結果（例如：reportWebVitals(console.log)）
 // 或發送到分析端點。了解更多：https://bit.ly/CRA-vitals
-reportWebVitals(); 
+reportWebVitals();
